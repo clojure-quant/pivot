ec019194b7232b825cde8f228eb4ce56babbb1b6
awb99 <awb99@gmail.com>
2025-05-16 17:01:37 -0500

    dojipivot algo

--- demo/src/demo/algo/dojipivot.clj
+++ demo/src/demo/algo/dojipivot.clj
@@ -1,4 +1,4 @@
-(ns demo.dojipivot
+(ns demo.algo.dojipivot
   (:require
    [tick.core :as t]
    [tablecloth.api :as tc]
@@ -21,46 +21,33 @@
    [demo.util :refer [print-ds]]
    [demo.env :refer [env]]))
 
-(def calendar [:forex :m])
 
-(def bars
-  (load-bars env {:asset "EURUSD"
-                  :window (w/create-trailing-window calendar 80000 (t/instant "2025-05-07T20:00:00Z"))}))
-
-
-(def dojis
-  (-> bars
-      (select-dojis-at-high-low {:min-wick-percent 0.7
-                                 :range-sma-n 60
-                                 :min-range-percent 1.0
-                                 :trailing-n 60})
-       (tc/select-rows (fn [row]  (> (:volume row) 100)))
-      
-      ))
-
-
-dojis
-
-(def pivots
-  (calc-multi-window
-   env
-   {:asset "EURUSD"
-    :calendar calendar
-    :windows [240 480 720 1440
-            ;4h 8h  12h 24h
-              ]
-    :window (w/create-trailing-window calendar 80000 (t/instant "2025-05-07T20:00:00Z"))}))
-
-(def n-pivots (only-highest-n-per-date pivots))
-
-n-pivots
-
-(plot-pivots-n-colored env n-pivots)
+(defn calc [asset opts]
+  (let [calendar [:forex :m]
+        calendar-doji [:forex :m10]
+        bars (load-bars env {:asset asset
+                             :window (w/create-trailing-window calendar-doji 80000 (t/instant "2025-05-07T20:00:00Z"))})
+        dojis (-> bars
+                  (select-dojis-at-high-low opts)
+                  ;(tc/select-rows (fn [row]  (> (:volume row) 100)))
+                  )
+        pivots (calc-multi-window
+                env
+                {:asset asset
+                 :calendar calendar
+                 :windows [240 480 720 1440
+                    ;4h 8h  12h 24h
+                           ]
+                 :window (w/create-trailing-window calendar 80000 (t/instant "2025-05-07T20:00:00Z"))})
+        n-pivots (only-highest-n-per-date pivots)]
+    ;
+    {:dojis dojis
+     :pivots n-pivots}))
 
 
 (defn plot-pivots-n-colored-doji
   "plots a dataset of pivot points (with different colors for different n levels)"
-  [env pivots-ds dojis-ds]
+  [env {:keys [dojis pivots]}]
   (plot/vegalite
    env
    {:spec {:width 1200
@@ -68,7 +55,8 @@ n-pivots
            :layer [; pivot
                    {:data {:name "pivots"}
                     :mark {:type "point" :filled true}
-                    :encoding {:x {:field "idx" :type "quantitative"}
+                    :encoding {;:x {:field "idx" :type "quantitative"}
+                               :x {:field "date" :type "temporal"}
                                :y {:field "pivot-price" :type "quantitative"
                                    :scale {:zero false}}
                                :color {:field "n" :type "ordinal"
@@ -82,18 +70,19 @@ n-pivots
                    {:data {:name "dojis"}
                     :mark {:type "rule"
                            ;:type "point" 
-                           
+
                            ;:shape "cross"
                            ;:strokeWidth 1        ;; <- This sets the line width
                            ;:width 1
                            ;:size 30
                            }
-                    :encoding {:x {:field "idx" :type "quantitative"}
+                    :encoding {;:x {:field "idx" :type "quantitative"}
+                               :x {:field "date" :type "temporal"}
                                ;:y {:field "close" :type "quantitative" :scale {:zero false}}
                                :y {:field "doji-marker-low" :type "quantitative" :scale {:zero false}}
                                :y2 {:field "doji-marker-high" :type "quantitative" :scale {:zero false}}
                                :color {:value "red"}
-                             
+
                                :tooltip [{:field "dt-str" :type "nominal"}
                                          {:field "open" :type "quantitative"}
                                          {:field "high" :type "quantitative"}
@@ -102,14 +91,25 @@ n-pivots
                                          {:field "doji-marker-low" :type "quantitative"}
                                          {:field "doji-marker-high" :type "quantitative"}
                                          {:field "doji-marker-type" :type "nominal"}
-                                         {:field "volume" :type "quantitative"}
-                                         ]}}]}
-    :data {:pivots (plot/convert-data (add-dt-str pivots-ds) [:pivot-price :idx :n
-                                                              :pivot-type  :pivot-range  :pivot-volume :dt-str])
-           :dojis (plot/convert-data (add-dt-str dojis-ds) [:close :idx :doji-marker-high :doji-marker-low :doji-marker-type
-                                                            :dt-str :volume :open :high :low])}}))
+                                         {:field "volume" :type "quantitative"}]}}]}
+    :data {:pivots (plot/convert-data (add-dt-str pivots) [:pivot-price :idx :date :n
+                                                           :pivot-type  :pivot-range  :pivot-volume :dt-str])
+           :dojis (plot/convert-data (add-dt-str dojis) [:close :idx :date :doji-marker-high :doji-marker-low :doji-marker-type
+                                                         :dt-str :volume :open :high :low])}}))
 
 
+(defn calc-plot [asset opts]
+  (->> (calc asset opts)
+       (plot-pivots-n-colored-doji env)))
 
 
-(plot-pivots-n-colored-doji env n-pivots dojis)
+(comment
+  (calc-plot "EURUSD"
+             {:min-wick-percent 0.7
+              :range-sma-n 6
+              :min-range-percent 0.7
+              :trailing-n 6})
+  
+  ;
+  )
+
